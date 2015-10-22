7d2582581e39713df9bc45df9d1bbdacfcac25c7
David Abrahams <david.abrahams@students.olin.edu>
2015-10-22 13:55:24 -0400

    removed duplicate files

--- src/get_players.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('../data/projections.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-    	if (row[0] == 'Player Name '):
-    		pass
-    	else:
-        	print row
